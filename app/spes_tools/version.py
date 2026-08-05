72d6444caa7514dfa18a33b4ef3c1d7466d2e145
spes mestre <presidente@spesginnasticamestre.it>
2026-08-05 14:20:03 +0200

    Update version.py

--- app/spes_tools/version.py
+++ app/spes_tools/version.py
@@ -1,3 +1,3 @@
 APP_NAME = "SPES Configuratore Contabile"
-APP_VERSION = "4.0"
-ORGANIZATION_NAME = "ASD SPES Mestre Ginnastica"
+APP_VERSION = "4.1"
+ORGANIZATION_NAME = "SPES Mestre Ginnastica A.S.D."
